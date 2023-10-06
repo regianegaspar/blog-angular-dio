1411488563ddfcbbb8742d9524ff53b31e60a122
Regiane Gaspar <123254220+reggaspar@users.noreply.github.com>
2023-10-06 18:36:03 -0300

    projeto completo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+    "id":"1",
+    "title":"Pré-venda do livro Almanaque Mágico de Harry Potter",
+    "description":"Originalmente intitulado The Harry Potter Wizarding Almanac, o livro vai chegar ao Brasil pela editora Rocco com o título Harry Potter: O Almanaque Mágico, e contará com um compêndio de listas, gráficos e mapas com ilustrações de sete artistas. O lançamento está previsto para o dia 10 de outubro e acontecerá simultaneamente em 34 línguas.",
+    "photoCover":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgqL3w1dvuAEJIAFG1zicQ4ISO7Acom6A2pnyheME69kBkVlU1CXchEUDUOqvYa3jrppPvm080jsKWxeZNYEhb3hEA9Y5U592uKvUEC28xJtNFH7PAHSZ4_k7dI8rK-OojXl6ohxlMnG3ZX_VCuBmyT_WVl6LHoCCZMzggJKbSOdejoq9HzMLCWafKK9fs8/w1200-h675-p-k-no-nu/Harry-Potter-O-Alamanaque-Magico.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Jogo de Harry Potter quebra recorde mesmo antes do lançamento",
+        "description":"Hogwarts Legacy é baseado em Harry Potter, mas se passa no final dos anos 1800, bem antes do período retratado nos livros, e abre o universo da franquia para além do Castelo de Hogwarts. Os jogadores são avatares de bruxas ou magos que completam missões para ganhar habilidades como voar em uma vassoura.",
+        "photoCover":"https://t.ctcdn.com.br/1sowh9w23vU0hLhvjjDJQm_yMbk=/1200x675/smart/i574923.jpeg"
+    },
+    {
+        "id":"3",
+        "title":"Primeira edição de Harry Potter é leiloada por R$66,5 mil",
+        "description":"Um exemplar antigo e gasto – e comprado por menos de R$ 2 – da primeira edição de “Harry Potter e a Pedra Filosofal“, de J.K. Rowling, foi vendido por US$ 13,56 mil em um leilão, cerca de R$ 66,5 mil. Publicado pela editora Bloomsbury em 1997, o livro é uma das 500 primeiras impressões da primeira edição antes de se transformar em um dos maiores fenômenos da literatura.O antigo dono era um colecionador que morreu no início do ano e havia comprado o livro pelo equivalente a R$ 1,88 em Wolverhampton, Inglaterra, de um lote de 300 que foram enviados para bibliotecas.",
+        "photoCover":"https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/07/skynews-harry-potter-auction_6215500-1.jpg?w=1220&h=674&crop=1"
+    },
+    {
+        "id":"4",
+        "title":"Série de televisão de Harry potter é anunciada oficialmente",
+        "description":"Acaba de ser anunciado oficialmente pelo grupo Warner Bros. Discovery que os livros de “Harry Potter” serão adaptados para a sua nova plataforma de streaming. De acordo com a Warner Bros. Discovery, cada temporada da série de televisão será baseada em um dos livros, e contará com um elenco totalmente novo em relação aos filmes.",
+        "photoCover":"https://i0.wp.com/animagos.com.br/wp-content/uploads/2023/04/editadaigor.png?resize=810%2C422&ssl=1"
+    }
+
+]
